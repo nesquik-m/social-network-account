0478130ac66484f49261d1be3e2dee8ae8b7b500
nesquik-m <nesti_lash@mail.ru>
2024-09-06 22:47:17 +0300

    created AccountUpdateFactory

---
+++ src/main/java/ru/skillbox/mapper/AccountUpdateFactory.java
@@ -0,0 +1,51 @@
+package ru.skillbox.mapper;
+
+import ru.skillbox.dto.AccountDto;
+import ru.skillbox.entity.Account;
+
+public class AccountUpdateFactory {
+
+    public static void updateFields(Account updatedAccount, AccountDto accountDto) {
+
+        // id
+        // isBlocked;
+        // isDeleted;
+        // isOnline;
+        // createdOn;
+        // updatedOn;
+
+        if (accountDto.getEmail() != null && !accountDto.getEmail().isEmpty()) {
+            updatedAccount.setEmail(accountDto.getEmail());
+        }
+        if (accountDto.getCity() != null && !accountDto.getCity().isEmpty()) {
+            updatedAccount.setCity(accountDto.getCity());
+        }
+        if (accountDto.getCountry() != null && !accountDto.getCountry().isEmpty()) {
+            updatedAccount.setCountry(accountDto.getCountry());
+        }
+        if (accountDto.getFirstName() != null && !accountDto.getFirstName().isEmpty()) {
+            updatedAccount.setFirstName(accountDto.getFirstName().trim());
+        }
+        if (accountDto.getLastName() != null && !accountDto.getLastName().isEmpty()) {
+            updatedAccount.setLastName(accountDto.getLastName().trim());
+        }
+        if (accountDto.getBirthDate() != null) {
+            updatedAccount.setBirthDate(accountDto.getBirthDate());
+        }
+        if (accountDto.getEmojiStatus() != null) {
+            updatedAccount.setEmojiStatus(accountDto.getEmojiStatus());
+        }
+        if (accountDto.getPhone() != null && !accountDto.getPhone().isEmpty()) {
+            updatedAccount.setPhone(accountDto.getPhone());
+        }
+        if (accountDto.getAbout() != null && !accountDto.getAbout().isEmpty()) {
+            updatedAccount.setAbout(accountDto.getAbout());
+        }
+        if (accountDto.getPhoto() != null && !accountDto.getPhoto().isEmpty()) {
+            updatedAccount.setPhoto(accountDto.getPhoto());
+        }
+        if (accountDto.getProfileCover() != null && !accountDto.getProfileCover().isEmpty()) {
+            updatedAccount.setProfileCover(accountDto.getProfileCover());
+        }
+    }
+}
--- src/main/java/ru/skillbox/service/impl/AccountServiceImpl.java
+++ src/main/java/ru/skillbox/service/impl/AccountServiceImpl.java
@@ -17,6 +17,7 @@ import ru.skillbox.entity.Account;
 import ru.skillbox.exception.AccountNotFoundException;
 import ru.skillbox.exception.AlreadyExistsException;
 import ru.skillbox.exception.BadRequestException;
+import ru.skillbox.mapper.AccountUpdateFactory;
 import ru.skillbox.repository.AccountRepository;
 import ru.skillbox.repository.specification.AccountSpecification;
 import ru.skillbox.service.AccountService;
@@ -69,49 +70,8 @@ public class AccountServiceImpl implements AccountService {
     @LogAspect(type = LogType.SERVICE)
     public AccountDto updateAccount(AccountDto accountDto) { // TODO: Security
         Account updatedAccount = getAccountById(testUUID);
-
-        // id
-        // isBlocked;
-        // isDeleted;
-        // isOnline;
-        // createdOn;
-        // updatedOn;
-
-        if (accountDto.getEmail() != null && !accountDto.getEmail().isEmpty()) {
-            updatedAccount.setEmail(accountDto.getEmail());
-        }
-        if (accountDto.getCity() != null && !accountDto.getCity().isEmpty()) {
-            updatedAccount.setCity(accountDto.getCity());
-        }
-        if (accountDto.getCountry() != null && !accountDto.getCountry().isEmpty()) {
-            updatedAccount.setCountry(accountDto.getCountry());
-        }
-        if (accountDto.getFirstName() != null && !accountDto.getFirstName().isEmpty()) {
-            updatedAccount.setFirstName(accountDto.getFirstName().trim());
-        }
-        if (accountDto.getLastName() != null && !accountDto.getLastName().isEmpty()) {
-            updatedAccount.setLastName(accountDto.getLastName().trim());
-        }
-        if (accountDto.getBirthDate() != null) {
-            updatedAccount.setBirthDate(accountDto.getBirthDate());
-        }
-        if (accountDto.getEmojiStatus() != null) {
-            updatedAccount.setEmojiStatus(accountDto.getEmojiStatus());
-        }
-        if (accountDto.getPhone() != null && !accountDto.getPhone().isEmpty()) {
-            updatedAccount.setPhone(accountDto.getPhone());
-        }
-        if (accountDto.getAbout() != null && !accountDto.getAbout().isEmpty()) {
-            updatedAccount.setAbout(accountDto.getAbout());
-        }
-        if (accountDto.getPhoto() != null && !accountDto.getPhoto().isEmpty()) {
-            updatedAccount.setPhoto(accountDto.getPhoto());
-        }
-        if (accountDto.getProfileCover() != null && !accountDto.getProfileCover().isEmpty()) {
-            updatedAccount.setProfileCover(accountDto.getProfileCover());
-        }
+        AccountUpdateFactory.updateFields(updatedAccount, accountDto);
         updatedAccount.setUpdatedOn(LocalDateTime.now());
-
         return AccountMapper.accountToAccountDto(accountRepository.save(updatedAccount));
     }
 
