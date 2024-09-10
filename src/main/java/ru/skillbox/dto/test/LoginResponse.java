a1c5c4836ad6788db8cad4b02e8c23f8d48f6685
nesquik-m <nesti_lash@mail.ru>
2024-09-11 00:57:34 +0300

    cors

--- src/main/java/ru/skillbox/dto/test/LoginResponse.java
+++ src/main/java/ru/skillbox/dto/test/LoginResponse.java
@@ -1,15 +1,15 @@
-package ru.skillbox.dto.test;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class LoginResponse {
-    private long timestamp;
-    private String data;
-}
+//package ru.skillbox.dto.test;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//public class LoginResponse {
+//    private long timestamp;
+//    private String data;
+//}
